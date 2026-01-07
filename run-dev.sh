da8e1907efa2adbc4016a956497bb9c7587fc5f3
fgeqma <fgeqma@gmail.com>
2026-01-07 04:19:42 -0500

    mute

--- run-dev.sh
+++ run-dev.sh
@@ -1,14 +1,11 @@
 concurrently \
   "npm run start --prefix ./client" \
-  "./nginx-1.27.3/sbin/nginx -p ./nginxAssetsServer -c nginx.conf" \
   "npm run dev --prefix ./mediasoupServer" \
   "./nginx-1.27.3/sbin/nginx -p ./mediasoupServer/nginx -c nginx.conf" \
   "npm run dev --prefix ./tableStaticContentServer" \
   "./nginx-1.27.3/sbin/nginx -p ./tableStaticContentServer/nginx -c nginx.conf" \
   "npm run dev --prefix ./userStaticContentServer" \
   "./nginx-1.27.3/sbin/nginx -p ./userStaticContentServer/nginx -c nginx.conf" \
-  "npm run dev --prefix ./liveTextEditingServer" \
-  "./nginx-1.27.3/sbin/nginx -p ./liveTextEditingServer/nginx -c nginx.conf" \
   "npm run dev --prefix ./videoServer" \
   "./nginx-1.27.3/sbin/nginx -p ./videoServer/nginx -c nginx.conf" \
   "npm run dev --prefix ./cephServer" \
