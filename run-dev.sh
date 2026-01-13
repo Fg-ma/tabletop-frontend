427d6e4f0aeb56b95bd601879ba243137d4f1f75
fgeqma <fgeqma@gmail.com>
2026-01-13 00:52:18 -0500

    mute

--- run-dev.sh
+++ run-dev.sh
@@ -3,8 +3,6 @@ concurrently \
   "npm run dev --prefix ./mediasoupServer" \
   "./nginx-1.27.3/sbin/nginx -p ./mediasoupServer/nginx -c nginx.conf" \
   "npm run dev --prefix ./tableStaticContentServer" \
-  "./nginx-1.27.3/sbin/nginx -p ./tableStaticContentServer/nginx -c nginx.conf" \
-  "npm run dev --prefix ./userStaticContentServer" \
   "./nginx-1.27.3/sbin/nginx -p ./userStaticContentServer/nginx -c nginx.conf" \
   "npm run dev --prefix ./videoServer" \
   "./nginx-1.27.3/sbin/nginx -p ./videoServer/nginx -c nginx.conf" \
